1df87b4a00674f1afc0918adf49bac87f5df7bfc
Taylor <taylor.m.white@hotmail.com>
2015-04-07 13:06:21 -0400

    Game activity will start now.
    the chosen difficulty is passed to the game activity. Game logic will be implemented dependant on level.

--- app/src/main/java/com/turnup/cs389team6/activities/MathOptionsActivity.java
+++ app/src/main/java/com/turnup/cs389team6/activities/MathOptionsActivity.java
@@ -1,8 +1,11 @@
 package com.turnup.cs389team6.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -12,18 +15,25 @@ import com.turnup.cs389team6.R;
  * Created by Taylor White on 3/26/2015.
  */
 public class MathOptionsActivity extends Activity {
+
+    Button startGame;
+    int selectedLevel=0;
     @Override
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gameoptions);
+        setContentView(R.layout.activity_game_options);
+        startGame = (Button) findViewById(R.id.startgame);
     }
     public void mathButtons(View v){
         boolean checked = ((RadioButton) v).isChecked();
         switch (v.getId()){
-            case R.id.radioButton:
+            case R.id.Level1:
                 Toast.makeText(this,"You have selected difficulty 1",Toast.LENGTH_SHORT).show();
+                selectedLevel = 1;
                 break;
-            case R.id.radioButton2:
+            case R.id.Level2:
                 Toast.makeText(this,"You have selected difficulty 2",Toast.LENGTH_SHORT).show();
                 break;
 
@@ -33,5 +43,16 @@ public class MathOptionsActivity extends Activity {
 
 
 
+    }
+    public void playGame(View v){
+
+        if(selectedLevel == 0){
+            Toast.makeText(this,"Please selected a difficulty",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Intent playGame = new Intent(this,GamePlay.class);
+            playGame.putExtra("LEVEL",selectedLevel);
+            startActivity(playGame);
+        }
     }
 }
