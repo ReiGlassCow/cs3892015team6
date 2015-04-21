556b4598dba1fad35ebc5958e44892cbc7d0b2a8
Dheer Mirchandani <dheerm@gmail.com>
2015-04-21 11:37:08 -0400

    Added game engine.

--- app/src/main/java/com/turnup/cs389team6/activities/GameActivity.java
+++ app/src/main/java/com/turnup/cs389team6/activities/GameActivity.java
@@ -1,7 +1,12 @@
 package com.turnup.cs389team6.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.turnup.cs389team6.R;
 import com.turnup.cs389team6.constants.Difficulty;
@@ -17,13 +22,17 @@ public class GameActivity extends BaseActivity {
     private Random rand;
     private TextView problem, chosenLevel;
     private int problemAnswer;
-    private int[] possibleAnswers = new int[6];
+    private Integer[] possibleAnswers = new Integer[6];
+
+    private GridView mGridView;
+    private int questionCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         selectedDifficulty = (Difficulty) getIntent().getSerializableExtra(MathOptionsActivity.EXTRA_DIFFICULTY);
         setContentView(R.layout.activity_game_layout);
+        mGridView = (GridView) findViewById(R.id.grid_view);
         chosenLevel = (TextView) findViewById(R.id.difficulty_textview);
         chosenLevel.setText(getString(R.string.level_title, selectedDifficulty.ordinal() + 1));
         problem = (TextView) findViewById(R.id.problem_textview);
@@ -41,6 +50,11 @@ public class GameActivity extends BaseActivity {
             secondOperand = rand.nextInt(selectedDifficulty.getMaxValue());
         }
         String operationSign = "";
+        if(secondOperand > firstOperand){
+            int temp = secondOperand;
+            secondOperand = firstOperand;
+            firstOperand = secondOperand;
+        }
         switch (op){
             case ADD:
                 problemAnswer = firstOperand + secondOperand;
@@ -55,11 +69,6 @@ public class GameActivity extends BaseActivity {
                 operationSign = " x ";
                 break;
             case DIVISION:
-                if(secondOperand > firstOperand){
-                    int temp = secondOperand;
-                    secondOperand = firstOperand;
-                    firstOperand = secondOperand;
-                }
                 if(firstOperand % secondOperand != 0){
                     secondOperand += firstOperand % secondOperand;
                 }
@@ -80,11 +89,36 @@ public class GameActivity extends BaseActivity {
         possibleAnswers[answerPos] = problemAnswer;
         for(int i = 0; i < possibleAnswers.length; i++){
             if(possibleAnswers[i] == Integer.MAX_VALUE){
-                //generate answer randomly with w/e u want
-                //possibleAnswers[i] = (generatedWrongAnswer)
+               int possibleAnswer = problemAnswer;
+               do{
+                   possibleAnswer = rand.nextInt(selectedDifficulty.getMaxValue() * 3);
+               } while(possibleAnswer == problemAnswer);
+               possibleAnswers[i] =  possibleAnswer;
+               //possibleAnswers[i] = (generatedWrongAnswer)
             }
         }
-        //setupui
+        setupGridAdapter();
+    }
+
+    private void setupGridAdapter(){
+        final ArrayAdapter<Integer> mGridAdapter = new ArrayAdapter<Integer>(this, R.layout.item_answer_choice, R.id.answer_item_textview, possibleAnswers);
+        mGridView.setAdapter(mGridAdapter);
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                questionCounter++;
+                //edit Questions Left Value;
+                //animate text of score.
+                if(mGridAdapter.getItem(position).intValue() == problemAnswer){
+                    Toast.makeText(GameActivity.this, "Correct Answer!", Toast.LENGTH_SHORT).show();
+                    //correct. animate score up
+                } else {
+                    Toast.makeText(GameActivity.this, "Wrong Answer", Toast.LENGTH_SHORT).show();
+                    //wrong animate score down
+                }
+                generateProblem();
+            }
+        });
     }
 
 
