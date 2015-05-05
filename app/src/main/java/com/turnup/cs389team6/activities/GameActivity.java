8d57016f043f2e863c2bac564e3a6daa7a3f8301
Taylor White <Taylor.m.white@hotmail.com>
2015-05-04 23:19:09 -0400

    added statistics popup window. Will now tell you how many you answered correctly and how many you answered incorrectly.
    The lovely sounds will also let you know how you're doing.
    
    :)
    
    Turn up for the presentation tomorrow.
    
    dont forget to look fly.

--- app/src/main/java/com/turnup/cs389team6/activities/GameActivity.java
+++ app/src/main/java/com/turnup/cs389team6/activities/GameActivity.java
@@ -6,7 +6,6 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.*;
@@ -16,6 +15,8 @@ import com.turnup.cs389team6.constants.Difficulty;
 import com.turnup.cs389team6.constants.Operation;
 
 import java.util.Random;
+
+import static android.graphics.Color.*;
 
 public class GameActivity extends BaseActivity {
 
@@ -35,16 +36,15 @@ public class GameActivity extends BaseActivity {
     private boolean isEndlessMode = false;
     private ImageView stats;
     Point p;
-    PopupWindow popUp;
-    LinearLayout layout;
-    TextView tv;
-    LayoutParams params;
-    LinearLayout mainLayout;
-    Button but;
-    boolean click = true;
+    private int[] correct = new int[3];
+    private int[] wrong = new int[3];
+    private TextView l1,l2;
 
 
+    LinearLayout layoutOfPopup;
+    PopupWindow popupStats;
 
+    TextView statistics;
 
 
 
@@ -59,6 +59,10 @@ public class GameActivity extends BaseActivity {
 
         stats = (ImageView) findViewById(R.id.statistics);
 
+
+
+        init();
+        popupInit();
 
 
         //Media sounds
@@ -76,20 +80,30 @@ public class GameActivity extends BaseActivity {
         problem = (TextView) findViewById(R.id.problem_textview);
         rand = new Random();
 
+        //Statistics
+
+        correct[0]=0; correct[1]=0; correct[2]=0;
+        wrong[0]=0; wrong[1]=0; wrong[2]=0;
+
 
         stats.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (p != null){
-                    showPopup(GameActivity.this,p);
+                if (p != null && !popupStats.isShowing()){
+                    statistics.setText("Correct : " + correct[0] + "\nWrong: " + wrong[0]);
 
+                    showPopup(GameActivity.this, p);
+
+                }
+                else{
+                    popupStats.dismiss();
                 }
             }
         });
 
 
 
-       // timerText.startAnimation(AnimationUtils.loadAnimation(GameActivity.this, android.R.anim.slide_in_left));
+
 
         timer = new CountDownTimer(totalTime,1000){
 
@@ -112,7 +126,23 @@ public class GameActivity extends BaseActivity {
         generateProblem();
     }
 
+    public void init() {
+        statistics = new TextView(this);
+        layoutOfPopup = new LinearLayout(this);
+        layoutOfPopup.setBackgroundColor(getResources().getColor(R.color.logo_yellow));
+        statistics.setText("Level 1:\n"+correct[0]);
+        statistics.setGravity(1);
+        statistics.setPadding(0, 0, 0, 20);
+        layoutOfPopup.addView(statistics);
+    }
 
+    public void popupInit() {
+
+
+        popupStats = new PopupWindow(layoutOfPopup, LayoutParams.FILL_PARENT,
+        LayoutParams.WRAP_CONTENT);
+        popupStats.setContentView(layoutOfPopup);
+    }
 
 
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -120,11 +150,10 @@ public class GameActivity extends BaseActivity {
         int[] location = new int[2];
 
 
-        // Get the x, y location and store it in the location[] array
-        // location[0] = x, location[1] = y.
+
         stats.getLocationOnScreen(location);
 
-        //Initialize the Point with x, and y positions
+
         p = new Point();
         p.x = location[0];
         p.y = location[1];
@@ -134,26 +163,12 @@ public class GameActivity extends BaseActivity {
 
     private void showPopup(GameActivity gameActivity, Point p) {
 
-        int popUpWidth = 400;
-        int popUpHeight = 350;
+        int OFFSET_X = 0;
+        int OFFSET_Y = 250;
 
-        LinearLayout viewGroup = (LinearLayout) gameActivity.findViewById(R.id.popup);
-        LayoutInflater layoutInflater = (LayoutInflater) gameActivity
-                .getSystemService(gameActivity.LAYOUT_INFLATER_SERVICE);
-        View layout = layoutInflater.inflate(R.layout.stats, viewGroup);
-
-        final PopupWindow popup = new PopupWindow(gameActivity);
-        popup.setContentView(layout);
-        popup.setWidth(popUpWidth);
-        popup.setHeight(popUpHeight);
-        popup.setFocusable(true);
-
-        int OFFSET_X = 50;
-        int OFFSET_Y = 50;
-
-        popup.showAtLocation(layout, Gravity.CENTER, p.x + OFFSET_X, p.y + OFFSET_Y);
-
-
+popupStats.setWidth(300);
+popupStats.setHeight(300);
+popupStats.showAtLocation(layoutOfPopup, Gravity.CENTER, OFFSET_X, OFFSET_Y);
 
 
     }
@@ -171,11 +186,11 @@ public class GameActivity extends BaseActivity {
         timer = new CountDownTimer(totalTime, 1000) {
             public void onTick(long millisUntilFinished) {
                 totalTime = millisUntilFinished;
-                timerText.setTextColor(Color.GREEN);
+                timerText.setTextColor(GREEN);
 
                 timerText.setText("Time: " + (millisUntilFinished / 1000));
-                if((millisUntilFinished/1000)<=7){
-                    timerText.setTextColor(Color.RED);
+                if((millisUntilFinished/1000)<=5){
+                    timerText.setTextColor(RED);
                     countdown.start();
                 }
             }
@@ -296,9 +311,11 @@ public class GameActivity extends BaseActivity {
                 //animate text of score.
                 if(mGridAdapter.getItem(position).intValue() == problemAnswer){
 
-                    woo.start();
+
                     Toast.makeText(GameActivity.this, R.string.correct_answer, Toast.LENGTH_SHORT).show();
-                    view.setBackgroundColor(Color.GREEN);
+                    woo.start();
+                    correct[0]++;
+
                     scoreCounter += 50;
                     score.setText("Score: "+scoreCounter);
                     totalTime = 31000;
@@ -321,6 +338,7 @@ public class GameActivity extends BaseActivity {
 
                     Toast.makeText(GameActivity.this, R.string.wrong_answer, Toast.LENGTH_SHORT).show();
                     //wrong animate score down
+                    wrong[0]++;
                     scoreCounter -=25;
                     score.setText("Score: " +scoreCounter);
                     totalTime = 31000;
